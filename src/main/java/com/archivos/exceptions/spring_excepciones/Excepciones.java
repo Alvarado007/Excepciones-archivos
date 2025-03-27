7f959cd1230c4442f8887c69e1a654c71aa46f2a
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-03-27 13:01:49 -0500

    incializacion excepciones en archivos

--- src/main/java/com/archivos/exceptions/spring_excepciones/Excepciones.java
+++ src/main/java/com/archivos/exceptions/spring_excepciones/Excepciones.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringExcepcionesApplication {
+public class Excepciones {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringExcepcionesApplication.class, args);
+		SpringApplication.run(Excepciones.class, args);
 	}
 
 }
