57166af4f4763bd821b6a377c9cb1d2818647451
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-03-27 12:34:49 -0500

    inicializacion

---
+++ src/test/java/com/archivos/exceptions/spring_excepciones/SpringExcepcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archivos.exceptions.spring_excepciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExcepcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
